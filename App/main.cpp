45f5d71d702fa9b1697b943fd19347709e3b8530
Orestis Tsakiridis <otsakiridis@freemail.gr>
2023-05-25 09:53:29 +0300

    Command-line parsing - ref #63
    Parse commandline options that determing 'task' to execute in CLI mode
    and show confirmation dialog. Namely '-r' and '-c'.

--- App/main.cpp
+++ App/main.cpp
@@ -2,21 +2,44 @@
 
 
 #include <QApplication>
+#include <QCommandLineParser>
 #include <QObject>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //parse command line
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption runOption(QStringList() << "r" << "run", "Run the specified task", "task name");
+    parser.addOption(runOption);
+    QCommandLineOption confirmOption(QStringList() << "c" << "confirm", "Show confirmation dialog");
+    parser.addOption(confirmOption);
+
+    parser.process(a);
+
+    if (parser.isSet(confirmOption) && !parser.isSet(runOption))
+    {
+        fputs(qPrintable("error: can't show confirmation in GUI mode. Maybe run with '-r' ?"), stderr);
+        fputs("\n\n", stderr);
+        fputs(qPrintable(parser.helpText()), stderr);
+        return 1;
+    }
+
+    if (parser.isSet(runOption))    // "CLI" mode
+    {
+        qDebug().noquote() << QString("running backup task '%1'...").arg(parser.value(runOption));
+        exit(0);
+    }
+
+    // "GUI" mode
     QIcon::setThemeName("Papirus");
     MainWindow w;
     w.show();
-
-//    QObject::connect(ui->actionE_xit, &QAction::triggered,  )
-//            connect(quitButton, &QPushButton::clicked, &app, &QCoreApplication::quit, Qt::QueuedConnection);
-
-
-    //QObject::connect(w, &MainWindow::PleaseQuit, &a, QCoreApplication::quit, Qt::QueuedConnection);
-
 
     return a.exec();
 }
