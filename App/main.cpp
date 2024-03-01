850766e9370b2ac7a2cc35eed1438e777c0802b7
Orestis Tsakiridis <otsakiridis@freemail.gr>
2024-03-01 18:23:51 +0200

    tidy up bootstraping in main.cpp

--- App/main.cpp
+++ App/main.cpp
@@ -37,10 +37,10 @@ int main(int argc, char *argv[])
     qDebug() << "Tasks in " << Lb::dataDirectory();
     qDebug() << "Application scripts in " << Lb::appScriptsDir();
 
-    AppContext appContext;
+
+    // default settings
     QSettings settings;
     settings.setValue("initialized", false);  // Remove settings file. Uncomment this to start afresh
-
     if ( ! settings.value("initialized", false).toBool())
     {
         qInfo() << "Blank settings found. They will get initialized.";
@@ -54,77 +54,24 @@ int main(int argc, char *argv[])
     settings.setValue("ApplicationFilePath", QApplication::applicationFilePath());
     qInfo() << "Settings file path:" << settings.fileName();
 
-    //parse command line
-//    QCommandLineParser parser;
-//    parser.addHelpOption();
-//    parser.addVersionOption();
-//    QCommandLineOption runOption(QStringList() << "r" << "run", "Run the specified task");
-//    parser.addOption(runOption);
-//    QCommandLineOption taskOption(QStringList() << "t" << "task", "Choose a task", "task name");
-//    parser.addOption(taskOption);
-
-//    parser.process(a);
-
-    QString taskName;
-//    if (parser.isSet(taskOption))
-//        taskName = parser.value(taskOption);
-
     TaskLoader taskLoader(Lb::dataDirectory());
-
-    /*
-    if (parser.isSet(runOption))    // "CLI" mode
-    {
-        if (parser.isSet(taskOption))
-        {
-            BackupModel persisted;
-            if (taskLoader.loadTask(taskName,persisted))
-            {
-                if (settings.value(Settings::TaskrunnerConfirmKey).toInt() == 2)
-                {
-                    QMessageBox messageBox(QMessageBox::Information, "Lisa Backup", QString("Backup task '%1' triggered. Shall i proceed ?").arg(taskName), QMessageBox::Yes | QMessageBox::Cancel, nullptr,Qt::Dialog);
-                    int ret = messageBox.exec();
-                    if (ret == QMessageBox::Yes) {
-                        qInfo().noquote() << QString("running backup task '%1'...").arg(taskName);
-                        QVector<QString> commands;
-                        Scripting::buildBackupCommands(persisted, commands);
-                        for (QString& command: commands)
-                        {
-                            qInfo().noquote() << command;
-                            QString out;
-                            Terminal::runShellCommand(command, &out);
-                            qInfo().noquote() << out;
-                        }
-                    }
-                }
-            } else
-            {
-                qCritical().noquote() << QString("error loading task '%1'...").arg(taskName);
-            }
-            exit(0);
-        } else
-        {
-            qCritical().noquote() << QString("no backup task specified. Use '-t'.");
-            exit(0);
-        }
-    }
-    */
 
     // "GUI" mode
     QIcon::setThemeName("Papirus");
+
+    AppContext appContext;
     TaskRunnerManager taskRunnerManager;
     appContext.taskRunnerManager = &taskRunnerManager;
-    MainWindow w(taskName, &appContext);
-
-    //qDebug() << "delete on close: " << triggerMonitorWindow.testAttribute(Qt::WA_DeleteOnClose);
+    MainWindow w(QString(), &appContext);
 
     // center within desktop
     QDesktopWidget *desktop = QApplication::desktop();
     QPoint windowOrigin((desktop->width()-w.width())/2, (desktop->height()-w.height())/2);
     w.move(windowOrigin);
 
+    // allow minimize-to-tray
     QApplication::setQuitOnLastWindowClosed(false);
 
     w.show();
-    //triggerMonitorWindow.show();
     return a.exec();
 }
