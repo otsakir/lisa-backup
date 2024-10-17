eaf5673335f7249f222d50301f341cd54f617e1b
Orestis Tsakiridis <otsakir@gmail.com>
2024-10-17 12:13:18 +0300

    version bump 0.4.0 -> 0.5.0 and small improvements in Debian packaging scripts
    relates to #62

--- App/conf.h
+++ App/conf.h
@@ -2,6 +2,6 @@
 #define CONF_H
 
 // do not change this line manually. It's overwritten by the version setting mechanism
-#define LBACKUP_VERSION "0.3.0"
+#define LBACKUP_VERSION "0.5.0"
 
 #endif // CONF_H
--- packaging/setversion.sh
+++ packaging/setversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -ri  "s/Version:\\s+.*/Version: $1/" lbackup/DEBIAN/control
+sed -ri  "s/Version:\\s+.*/Version: $1/" lbackup-vanilla/DEBIAN/control
 sed -ri "s/LBACKUP_VERSION\\s+.*/LBACKUP_VERSION \"$1\"/" ../App/conf.h
